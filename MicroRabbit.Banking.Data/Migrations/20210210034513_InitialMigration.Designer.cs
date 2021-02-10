cfc5a00ac6857da2fea05fe1514a29f9b3d1c98f
Rodolpho Villas Boas <rodolphovb@gmail.com>
2021-02-10 00:47:05 -0300

    migration database

--- MicroRabbit.Banking.Data/Migrations/20210210034513_InitialMigration.Designer.cs
+++ MicroRabbit.Banking.Data/Migrations/20210210034513_InitialMigration.Designer.cs
@@ -9,14 +9,14 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MicroRabbit.Banking.Data.Migrations
 {
     [DbContext(typeof(BankingDbContext))]
-    [Migration("20190609015054_Initial Migration")]
+    [Migration("20210210034513_InitialMigration")]
     partial class InitialMigration
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.2.4-servicing-10062")
+                .HasAnnotation("ProductVersion", "2.2.6-servicing-10079")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
